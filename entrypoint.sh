d6fa0ba98109bcf9400884f036d56175631ed4c5
Andrew Schwartz <andrew@3playmedia.com>
2023-01-21 11:37:30 -0500

    Support values input to action that is passed to helm (deploy only)

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,7 @@ set -e
 export APPLICATION_HELMFILE=$(pwd)/${HELMFILE_PATH}/${HELMFILE}
 
 source /etc/profile.d/aws.sh
- 
+
 # Used for debugging
 aws sts --region ${AWS_REGION} get-caller-identity
 
@@ -22,9 +22,13 @@ if [[ "${HELM_DEBUG}" == "true" ]]; then
 	DEBUG_ARGS=" --debug"
 fi
 
+if [[ -z "$HELM_VALUES" ]]; then
+  HELM_VALUES_FLAG="--set ${HELM_VALUES}"
+fi
+
 if [[ "${OPERATION}" == "deploy" ]]; then
 
-	OPERATION_COMMAND="helmfile --namespace ${NAMESPACE} --environment ${ENVIRONMENT} --file /deploy/helmfile.yaml $DEBUG_ARGS apply"
+	OPERATION_COMMAND="helmfile ${HELM_VALUES_FLAG} --namespace ${NAMESPACE} --environment ${ENVIRONMENT} --file /deploy/helmfile.yaml $DEBUG_ARGS apply"
 	echo "Executing: ${OPERATION_COMMAND}"
 	${OPERATION_COMMAND}
 
@@ -57,5 +61,3 @@ elif [[ "${OPERATION}" == "destroy" ]]; then
     fi
 	fi
 fi
-
-
