898eea2bbaa5b0d9c112f4eb785a28058cc31992
Andrew Schwartz <andrew@3playmedia.com>
2023-01-22 21:22:30 -0500

    fix superfluous flag arg

--- entrypoint.sh
+++ entrypoint.sh
@@ -36,7 +36,7 @@ fi
 if [[ "${OPERATION}" == "deploy" ]]; then
 	echo "Deploying..."
 
-	OPERATION_COMMAND="helmfile ${HELM_VALUES_FLAG} ${HELM_VALUES_FILE_FLAG} --namespace ${NAMESPACE} --environment ${ENVIRONMENT} --file /deploy/helmfile.yaml $DEBUG_ARGS apply"
+	OPERATION_COMMAND="helmfile ${HELM_VALUES_FLAG} --namespace ${NAMESPACE} --environment ${ENVIRONMENT} --file /deploy/helmfile.yaml $DEBUG_ARGS apply"
 	echo "Executing: ${OPERATION_COMMAND}"
 	${OPERATION_COMMAND}
 
